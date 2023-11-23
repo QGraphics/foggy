9787f2bc68291b815721730f259c7a3d8fff4dd4
Omar Elshenhabi <amrmawaheb65@gmail.com>
2023-11-24 01:20:18 +0300

    project init

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,12 @@
 //
 // Created by omar on 11/24/23.
 //
+
+#include <iostream>
+#include <ctime>
+
+int main() {
+    std::time_t result = std::time(nullptr);
+    std::cout << std::asctime(std::localtime(&result)) << std::endl;
+    return 0;
+}
